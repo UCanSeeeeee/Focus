e0b38ac9f30f30a9fb721c32490c76615ca209ae
ChiehWang <982304048@qq.com>
2023-06-25 14:34:53 +0800

    找回丢失文件 (+1 squashed commit) Squashed commits: [f1110ce] 回退准备

--- Bilibili/ViewModel/BNMainAppPlatformViewModel.mm
+++ Bilibili/ViewModel/BNMainAppPlatformViewModel.mm
@@ -6,9 +6,7 @@
 //
 
 #import "BNMainAppPlatformViewModel.h"
-#import "BNMainCardViewModel.h"
-#import "MMUIView.h"
-#import "BNAuthorDataInfo.h"
+#import "BNSubDataDefine.h"
 
 @interface BNMainAppPlatformViewModel ()
 
@@ -18,18 +16,10 @@
 
 @implementation BNMainAppPlatformViewModel
 
-- (instancetype)init {
-    if (self = [super init]) {
-        
-    }
-    return self;
-}
-
 - (NSMutableArray<NSNumber *> *)cardTypeArray {
     if (!_cardTypeArray) {
         _cardTypeArray = [NSMutableArray array];
         [_cardTypeArray addObject:@(BNSubAuthorPlatformTypeBliBli)];
-//        YouTube接口：
         [_cardTypeArray addObject:@(BNSubAuthorPlatformTypeYouTube)];
     }
     return _cardTypeArray;
